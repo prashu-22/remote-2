74624e57e981fe5c975888bb13dca0149531969d
prashu-22 <eerantiprashanth2@gmail.com>
2025-12-29 23:19:31 +0530

    public path went wrond

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].js",
-    publicPath: "/https://remote-2-prashu.vercel.app/",
+    publicPath: "https://remote-2-prashu.vercel.app/",
     clean: true,
   },
   module: {
